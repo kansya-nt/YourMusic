f49fc59fc8493137b005f2ddb112b8cb403cec1e
kansya-nt <149471959+kansya-nt@users.noreply.github.com>
2024-01-03 12:50:55 +0700

    A config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 7200))
 # Chat id of a group for logging bot's activities
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 
-# Get this value from @FallenxBot on Telegram by /id
+# Get this value from @Berlinmusicbot on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", 6024180996))
 
 ## Fill these variables if you're deploying on heroku.
@@ -32,15 +32,15 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/kansya-nt/YourMusic",
+    "https://github.com/kansya-nt/AnonXMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/asupanhot_viral")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/berlinmusic_support")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Berlinmusic_support")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Berlinmusic_support")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -52,7 +52,7 @@ SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", None)
 
 
 # Maximum limit for fetching playlist's track from youtube, spotify, apple links.
-PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 25))
+PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 00))
 
 
 # Telegram audio and video file size limit (in bytes)
@@ -61,7 +61,7 @@ TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 1073741824))
 # Checkout https://www.gbmb.org/mb-to-bytes for converting mb to bytes
 
 
-# Get your pyrogram v2 session from @StringFatherBot on Telegram
+# Get your pyrogram v2 session from @Berlingenstringbot on Telegram
 STRING1 = getenv("STRING_SESSION", None)
 STRING2 = getenv("STRING_SESSION2", None)
 STRING3 = getenv("STRING_SESSION3", None)
@@ -78,20 +78,21 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://graph.org/file/a5de6027104ca87c5b562.jpg"
+    "START_IMG_URL", "https://graph.org/file/b87a43a56db9f6e7e3835.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://graph.org/file/a5de6027104ca87c5b562.jpg"
-PLAYLIST_IMG_URL = "https://graph.org/file/a5de6027104ca87c5b562.jpg"
-STATS_IMG_URL = "https://graph.org/file/a5de6027104ca87c5b562.jpg"
-TELEGRAM_AUDIO_URL = "https://graph.org/file/a5de6027104ca87c5b562.jpg"
-TELEGRAM_VIDEO_URL = "https://graph.org/file/a5de6027104ca87c5b562.jpg"
-STREAM_IMG_URL = "https://graph.org/file/a5de6027104ca87c5b562.jpg"
-SOUNCLOUD_IMG_URL = "https://graph.org/file/a5de6027104ca87c5b562.jpg"
-YOUTUBE_IMG_URL = "https://graph.org/file/a5de6027104ca87c5b562.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://graph.org/file/a5de6027104ca87c5b562.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://graph.org/file/a5de6027104ca87c5b562.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://graph.org/file/a5de6027104ca87c5b562.jpg"
+    "PING_IMG_URL", "https://graph.org/file/b87a43a56db9f6e7e3835.jpg"
+)
+PLAYLIST_IMG_URL = "https://graph.org/file/b87a43a56db9f6e7e3835.jpg"
+STATS_IMG_URL = "https://graph.org/file/b87a43a56db9f6e7e3835.jpg"
+TELEGRAM_AUDIO_URL = "https://graph.org/file/b87a43a56db9f6e7e3835.jpg"
+TELEGRAM_VIDEO_URL = "https://graph.org/file/b87a43a56db9f6e7e3835.jpg"
+STREAM_IMG_URL = "https://graph.org/file/b87a43a56db9f6e7e3835.jpg"
+SOUNCLOUD_IMG_URL = "https://graph.org/file/b87a43a56db9f6e7e3835.jpg"
+YOUTUBE_IMG_URL = "https://graph.org/file/b87a43a56db9f6e7e3835.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://graph.org/file/b87a43a56db9f6e7e3835.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://graph.org/file/b87a43a56db9f6e7e3835.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://graph.org/file/b87a43a56db9f6e7e3835.jpg"
 
 
 def time_to_seconds(time):
@@ -109,7 +110,7 @@ if SUPPORT_CHANNEL:
         )
 
 if SUPPORT_CHAT:
-    if not re.match("(?:http|https)://", SUPPORT_CHAT):
+    if not re.match("(?:http|https)://", SUPPORT_CHANNEL):
         raise SystemExit(
             "[ERROR] - Your SUPPORT_CHAT url is wrong. Please ensure that it starts with https://"
         )
