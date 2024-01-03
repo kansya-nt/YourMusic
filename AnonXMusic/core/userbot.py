a14c58acc9a3670a7f53c6f01c206128961f7e99
kansya-nt <149471959+kansya-nt@users.noreply.github.com>
2024-01-03 12:00:18 +0700

    E userbot.py

--- AnonXMusic/core/userbot.py
+++ AnonXMusic/core/userbot.py
@@ -51,8 +51,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("mahadappa")
-                await self.one.join_chat("metahoe")
+                await self.one.join_chat("asupanhot_viral")
+                await self.one.join_chat("berlinmusic_support")
             except:
                 pass
             assistants.append(1)
@@ -72,8 +72,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("mahadappa")
-                await self.two.join_chat("metahoe")
+                await self.two.join_chat("asupanhot_viral")
+                await self.two.join_chat("berlinmusic_support")
             except:
                 pass
             assistants.append(2)
@@ -93,8 +93,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("mahadappa")
-                await self.three.join_chat("metahoe")
+                await self.three.join_chat("asupanhot_viral")
+                await self.three.join_chat("berlinmusic_support")
             except:
                 pass
             assistants.append(3)
@@ -114,8 +114,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("mahadappa")
-                await self.four.join_chat("metahoe")
+                await self.four.join_chat("asupanhot_viral")
+                await self.four.join_chat("berlinmusic_support")
             except:
                 pass
             assistants.append(4)
@@ -135,8 +135,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("mahadappa")
-                await self.five.join_chat("metahoe")
+                await self.five.join_chat("asupanhot_viral")
+                await self.five.join_chat("berlinmusic_support")
             except:
                 pass
             assistants.append(5)
